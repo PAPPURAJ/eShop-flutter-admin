9381c3608f57913b576c22bf5b39afced31a9078
pappuraj <pappuraj.duet@gmail.com>
2024-03-28 23:25:23 +0600

    Final

---
+++ lib/add_product.dart
@@ -0,0 +1,189 @@
+import 'dart:collection';
+import 'dart:io';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:onlineshop/common_tools/common_tools.dart';
+import 'package:onlineshop/common_tools/date_time.dart';
+import 'package:onlineshop/common_tools/firebase_api.dart';
+import 'package:onlineshop/common_tools/widget/button_widget.dart';
+
+File? imageFile;
+var _ID;
+var PHOTOGRAPH_FIRE="Product";
+var PHOTOGRAPH="Photograph";
+TextEditingController nameCont = TextEditingController();
+BuildContext? cnxt;
+
+class AddPhotographyMain extends StatelessWidget {
+  AddPhotographyMain(var ID) {
+    _ID = ID;
+  }
+
+  @override
+  Widget build(BuildContext context) => AddPhotography();
+}
+
+class AddPhotography extends StatefulWidget {
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<AddPhotography> {
+  UploadTask? task;
+  File? file;
+
+  final imgPicker = ImagePicker();
+  Future<void> showOptionsDialog(BuildContext context) {
+    return showDialog(
+        context: context,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: Text("Options"),
+            content: SingleChildScrollView(
+              child: ListBody(
+                children: [
+                  GestureDetector(
+                    child: Text("Capture Image From Camera"),
+                    onTap: () async {
+                      var imgCamera =
+                      await imgPicker.getImage(source: ImageSource.camera);
+                      setState(() {
+                        file = File(imgCamera!.path);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                  Padding(padding: EdgeInsets.all(10)),
+                  GestureDetector(
+                    child: Text("Take Image From Gallery"),
+                    onTap: () async {
+                      var imgGallery =
+                      await imgPicker.getImage(source: ImageSource.gallery);
+                      setState(() {
+                        file = File(imgGallery!.path);
+                      });
+                      Navigator.of(context).pop();
+                    },
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    cnxt = context;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Add product"),
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(32),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                GestureDetector(
+                  onTap: () => showOptionsDialog(context),
+                  child: Container(
+                    child: file != null
+                        ? Container(
+                      height: 300,
+                      width: 300,
+                      decoration: BoxDecoration(
+                          image: DecorationImage(
+                            image: FileImage(file!),
+                          )),
+                    )
+                        : Container(
+                      height: 200,
+                      width: 200,
+                      decoration: BoxDecoration(color: Colors.blueAccent),
+                      child: Image(
+                          image: AssetImage(
+                              'assets/images/select image.png')),
+                    ),
+                  ),
+                ),
+                Space(48),
+                TextField(
+                  controller: nameCont,
+                ),
+                Space(48),
+                ButtonWidget(
+                  text: 'Upload File',
+                  icon: Icons.cloud_upload_outlined,
+                  onClicked: () {
+                    uploadFile();
+                  },
+                ),
+                Space(20),
+                task != null ? buildUploadStatus(task!) : Container(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future uploadFile() async {
+    String fileName = getDateTimeString();
+    if (file == null) {
+      Fluttertoast.showToast(msg: "Please select a picture");
+      return;
+    }
+
+    final destination = PHOTOGRAPH_FIRE + '/$fileName';
+
+    task = FirebaseApi.uploadFile(destination, file!);
+    setState(() {});
+
+    if (task == null) return;
+
+    final snapshot = await task!.whenComplete(() {});
+    final urlDownload = await snapshot.ref.getDownloadURL();
+
+    print('Download-Link: $urlDownload');
+
+    Map<String, Object> data = HashMap();
+    data["URL"] = urlDownload;
+    data["Name"] = nameCont.text.isEmpty ? getDateTimeName() : nameCont.text;
+    data["FileName"] = fileName;
+    data["Date"] = getDate();
+    data['Time'] = getTime();
+
+    FirebaseFirestore.instance
+        .collection(PHOTOGRAPH_FIRE)
+        .add(data)
+        .whenComplete(() => {
+      Fluttertoast.showToast(msg: "Data added"),
+      Navigator.pop(context),
+    });
+  }
+
+  Widget buildUploadStatus(UploadTask task) => StreamBuilder<TaskSnapshot>(
+    stream: task.snapshotEvents,
+    builder: (context, snapshot) {
+      if (snapshot.hasData) {
+        final snap = snapshot.data!;
+        final progress = snap.bytesTransferred / snap.totalBytes;
+        final percentage = (progress * 100).toStringAsFixed(2);
+
+        return Text(
+          '$percentage %',
+          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        );
+      } else {
+        return Container();
+      }
+    },
+  );
+}
---
+++ lib/product_list.dart
@@ -0,0 +1,184 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+import 'package:onlineshop/add_product.dart';
+import 'package:onlineshop/common_tools/common_tools.dart';
+import 'package:onlineshop/common_tools/delete_feature.dart';
+
+String? ID;
+var PHOTOGRAPH_FIRE="Product";
+var PHOTOGRAPH="Photograph";
+class ProductMain extends StatefulWidget {
+
+  @override
+  _ProductState createState() => _ProductState();
+}
+
+class _ProductState extends State<ProductMain> {
+  @override
+  Widget build(BuildContext context) {
+    var reference = FirebaseFirestore.instance.collection(PHOTOGRAPH_FIRE);
+
+    //getData();
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddPhotography()));
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: Center(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Space(40),
+          const Text(
+            "Products",
+            style: TextStyle(
+              fontSize: 25,
+              fontWeight: FontWeight.w600,
+              color: Colors.white,
+            ),
+            textAlign: TextAlign.center,
+          ),
+          Space(20),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20,
+                vertical: 30,
+              ),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(30),
+                  topRight: Radius.circular(30),
+                ),
+              ),
+              child: StreamBuilder(
+                stream: reference
+                    .where('ID', isEqualTo: ID)
+                    .orderBy('FileName', descending: true)
+                    .snapshots(),
+                builder: (BuildContext context,
+                    AsyncSnapshot<QuerySnapshot> snapshot) {
+                  if (snapshot.hasData) {
+                    return GridView.count(
+                      crossAxisCount: 2,
+                      crossAxisSpacing: 10,
+                      mainAxisSpacing: 10,
+                      children: List.generate(snapshot.data!.size, (index) {
+                        // if (snapshot.data!.docs
+                        //         .map((e) => e['ID'])
+                        //         .elementAt(index) !=
+                        //     ID) return Container();
+
+                        return GestureDetector(
+                          onTap: () {
+                            // Navigator.push(
+                            //     context,
+                            //     MaterialPageRoute(
+                            //         builder: (context) => ViewPhoto(
+                            //             snapshot.data!.docs
+                            //                 .map((e) => e['URL'])
+                            //                 .elementAt(index),
+                            //             snapshot.data!.docs
+                            //                     .map((e) => e['Time'])
+                            //                     .elementAt(index) +
+                            //                 "  " +
+                            //                 snapshot.data!.docs
+                            //                     .map((e) => e['Date'])
+                            //                     .elementAt(index))));
+                          },
+                          onLongPress: () {
+                            deleteFireDialog(
+                                context,
+                                snapshot.data!.docs.elementAt(index).reference,
+                                snapshot.data!.docs
+                                    .map((e) => e['URL'])
+                                    .elementAt(index),
+                                () {});
+                          },
+                          child: Hero(
+                              tag: snapshot.data!.docs
+                                  .map((e) => e['URL'])
+                                  .elementAt(index),
+                              child: Stack(
+                                children: [
+                                  Container(
+                                    decoration: BoxDecoration(
+                                      borderRadius: BorderRadius.circular(15),
+                                      border: Border.all(
+                                        width: 3,
+                                        color: Colors.green,
+                                        style: BorderStyle.solid,
+                                      ),
+                                      image: DecorationImage(
+                                        image: NetworkImage(snapshot.data!.docs
+                                            .map((e) => e["URL"])
+                                            .elementAt(index)
+                                            .toString()),
+                                        fit: BoxFit.cover,
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 10,
+                                    right: 10,
+                                    child: Text(
+                                      snapshot.data!.docs
+                                              .map((e) => e['Time'])
+                                              .elementAt(index) +
+                                          " " +
+                                          snapshot.data!.docs
+                                              .map((e) => e['Date'])
+                                              .elementAt(index),
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 10,
+                                        color: Colors.white,
+                                        //backgroundColor: Color.fromRGBO(255, 255, 255, 150)
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    bottom: 25,
+                                    right: 10,
+                                    child: Text(
+                                      snapshot.data!.docs
+                                          .map((e) => e['Name'])
+                                          .elementAt(index),
+                                      style: const TextStyle(
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 12,
+                                        color: Colors.white,
+                                      ),
+                                    ),
+                                  )
+                                ],
+                              )),
+                        );
+                      }),
+                    );
+                  }
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      )),
+    );
+  }
+
+  Future<void> getData() async {
+    QuerySnapshot snap = await FirebaseFirestore.instance
+        .collection(PHOTOGRAPH_FIRE)
+        .where('ID < \'100\'')
+        .get();
+    print('=============== ${snap.size}');
+  }
+}
